f361fb3d7892a94a675e120d0e78d5c66e595896
SwapKul <skulshreshtha182@gmail.com>
2022-05-12 02:56:25 +0530

    updates

--- self_1/app.js
+++ self_1/app.js
@@ -1,9 +1,19 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
-app.get((req, res, next) => {
-    res.send("<h1>This is a random text.</h1>");
+const homeRoute = require('./routes/home');
+const contactRoute = require('./routes/contact-us');
+
+app.set('view engine', 'pug')
+
+app.use(homeRoute);
+app.use(contactRoute);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
---
+++ self_1/routes/contact-us.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../utils/path')
+
+const router = express.Router();
+
+router.get('/contact-us', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact-us.html'))
+})
+
+module.exports = router;
